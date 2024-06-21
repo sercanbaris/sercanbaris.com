53086f614607b65740f20af8e85c9927ab67fae5
Sercan Barış <sercanbaris@gmail.com>
2024-06-21 20:06:43 +0300

    sdds

--- src/App.js
+++ src/App.js
@@ -4,7 +4,34 @@ function App() {
   const [todos, setTodos] = useState([]);
   const [inputValue, setInputValue] = useState('');
 
-  const handleInputChange = (e) => {
+  const handleIn// MySQL modülünü içe aktar
+const mysql = require('mysql');
+
+// Bağlantı bilgilerini içeren bir nesne oluştur
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'your_username',
+  password: 'your_password',
+  database: 'your_database_name'
+});
+
+// Veritabanına bağlan
+connection.connect(err => {
+  if (err) {
+    console.error('Veritabanına bağlanırken bir hata oluştu: ' + err.stack);
+    return;
+  }
+  console.log('Veritabanına başarıyla bağlandı. Bağlantı ID\'si: ' + connection.threadId);
+});
+
+// Bağlantıyı kullanarak bir sorgu yap (örnek olarak bir tablodan veri çekme)
+connection.query('SELECT * FROM your_table_name', (err, results, fields) => {
+  if (err) throw err;
+  console.log(results);
+});
+
+// Bağlantıyı kapat
+connection.end();putChange = (e) => {
     setInputValue(e.target.value);
   };
 
