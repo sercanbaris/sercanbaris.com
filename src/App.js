a6ef8b6296a70a2898d407876f396ecf9f7aed35
Sercan Barış <sercanbaris@gmail.com>
2024-06-21 19:38:13 +0300

    fd

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
 
     useEffect(() => {
         axios.get('http://sercanbaris.com:3001/data')
+
             .then(response => {
                 setData(response.data);
             })
@@ -16,7 +17,7 @@ function App() {
 
     return (
         <div>
-            <h1>Data from MySQL</h1>
+            <h1>Data from MySQL Selam</h1>
             <ul>
                 {data.map(item => (
                     <li key={item.id}>{item.name}</li>
